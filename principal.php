ff60edc7c127c3f992e854509872fd1751fcc947
William Lucas <williamlucasiablonski@gmail.com>
2024-11-27 20:07:58 -0300

    Versão1

--- principal.php
+++ principal.php
@@ -1,4 +1,8 @@
 <?php include 'principal_controller.php'; ?>
+
+$produtos = getProdutos();
+?>
+
 <?php include 'header.php'; ?>
 
 <div class="flex-grow-1">
@@ -9,6 +13,22 @@
             <input type="submit" name="logout" value="Logout">
         </form>
     </div>
-
+    <div class="container p-2">
+    <?php foreach ($produtos as $produto): ?>    
+        <div class="card  float-left" style="width: 18rem;">
+            <img src="<?php echo $produto['url_img']; ?>" class="rounded mx-auto d-block" alt="Imagem do Produto" style="width: 100px;">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $produto['nome']; ?></h5>
+                <p class="card-text"><?php echo $produto['descricao']; ?></p>
+                <p><strong>PreÃ§o:</strong> R$ <?php echo number_format($produto['valorunitario'], 2, ',', '.'); ?></p>
+                <!-- FormulÃ¡rio para adicionar ao carrinho -->
+                <form method="POST" action="principal.php">
+                    <input type="hidden" name="id_produto" value="<?php echo $produto['id']; ?>">
+                    <button type="submit" name="adicionar_produto" class="btn btn-primary btn-block">Comprar</button>
+                </form>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
 
 <?php include 'footer.php'; ?>
--- principal_controller.php
+++ principal_controller.php
@@ -12,11 +12,53 @@ if (!isset($_SESSION['email'])) {
 $nome = $_SESSION['nome'];
 $email = $_SESSION['email'];
 
+function getProdutos() {
+    global $conn;
+    $result = $conn->query("SELECT * FROM produtos");
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+function adicionarAoCarrinho($id_produto) {
+    global $conn;
+    
+    // Verifica se o produto jÃ¡ estÃ¡ no carrinho
+    if (isset($_SESSION['carrinho'][$id_produto])) {
+        // Se jÃ¡ existe, aumenta a quantidade
+        $_SESSION['carrinho'][$id_produto]['quantidade']++;
+        $_SESSION['carrinho'][$id_produto]['subtotal'] = $_SESSION['carrinho'][$id_produto]['quantidade'] * $_SESSION['carrinho'][$id_produto]['preco'];
+    } else {
+        // Se nÃ£o existe, adiciona o item ao carrinho
+        $produto = getProdutoPorId($id_produto);
+        $_SESSION['carrinho'][$id_produto] = [
+            'id_produto'   => $produto['id'],
+            'nome_produto' => $produto['nome'],
+            'quantidade'   => 1,
+            'preco'        => $produto['valorunitario'],
+            'subtotal'     => $produto['valorunitario']
+        ];
+    }
+    header("Location: shopcart.php");
+    exit();
+}
+
+function getProdutoPorId($id_produto) {
+    global $conn;
+    $result = $conn->query("SELECT * FROM produtos WHERE id = $id_produto");
+    return $result->fetch_assoc();
+}
+
 // Função para lidar com o logout
 if (isset($_POST['logout'])) {
     session_unset();
     session_destroy();
     header("Location: index.php");
+    exit();
+}
+
+if (isset($_POST['adicionar_produto'])) {
+    $id_produto = (int)$_POST['id_produto'];
+    adicionarAoCarrinho($id_produto);
+    header("Location: principal.php");
     exit();
 }
 ?>
